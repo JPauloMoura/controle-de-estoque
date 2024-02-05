47335ff8e1eaca8c1017089415f5bb2b9f67b388
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-05 15:43:07 -0300

    fix: add pagination arguments

--- internal/product/services/product/handler_list_product.go
+++ internal/product/services/product/handler_list_product.go
@@ -4,10 +4,11 @@ import (
 	"log/slog"
 
 	"github.com/JPauloMoura/controle-de-estoque/internal/product/entity"
+	"github.com/JPauloMoura/controle-de-estoque/internal/product/repository"
 )
 
-func (p productService) ListProducts() ([]entity.Product, error) {
-	listProduct, err := p.database.GetAllProducts()
+func (p productService) ListProducts(pagination *repository.Pagination) ([]entity.Product, error) {
+	listProduct, err := p.database.GetAllProducts(pagination)
 	if err != nil {
 		slog.Error("failed to list products", err)
 		return nil, err
