897354dbbcb94f1320a09be6eccc470b2ef5a488
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-06 22:38:13 -0300

    refact: updade routes

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -9,8 +9,12 @@ import (
 	"github.com/JPauloMoura/controle-de-estoque/internal/product/handlers/rest"
 	"github.com/JPauloMoura/controle-de-estoque/internal/product/repository"
 	"github.com/JPauloMoura/controle-de-estoque/internal/product/services/product"
+	userHandler "github.com/JPauloMoura/controle-de-estoque/internal/user/handlers"
+	userRepo "github.com/JPauloMoura/controle-de-estoque/internal/user/repository"
+	"github.com/JPauloMoura/controle-de-estoque/pkg/auth"
 	"github.com/JPauloMoura/controle-de-estoque/pkg/loggers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
 )
 
@@ -20,16 +24,42 @@ func main() {
 	}
 
 	loggers.ConfigLogger()
-	router := buildApp()
+	handlers := buildHandlers()
 
 	slog.Info("server is running in port 3002...")
-	http.ListenAndServe(":3002", router)
+	http.ListenAndServe(":3002", handlers)
 }
 
-func buildApp() *chi.Mux {
-	repo := repository.NewProductRepository(database.ConnectDb())
-	svcProduct := product.NewProductService(repo)
-	router := rest.Handler(svcProduct)
+func buildHandlers() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RequestID)
+
+	db := database.ConnectDb()
+	productRepository := repository.NewProductRepository(db)
+	userRepository := userRepo.NewUserRepository(db)
+	productService := product.NewProductService(productRepository)
+
+	{ // user handlers
+		h := userHandler.NewHandlerUser(userRepository)
+		router.Post("/user", h.CreateUser)
+		router.Post("/user/login", h.Login)
+	}
+
+	{ // product handlers
+		h := rest.NewHandlerProduct(productService)
+		router.Group(func(r chi.Router) {
+			r.Use(auth.MiddlewareAuth)
+
+			r.Get("/products", h.ListProducts)
+			r.Get("/products/{id}", h.GetProduct)
+			r.Post("/products", h.CreateProduct)
+			r.Put("/products/{id}", h.UpdateProduct)
+			r.Delete("/products/{id}", h.DeleteProduct)
+
+		})
+	}
 
 	return router
 }
--- internal/product/handlers/rest/handler.go
+++ internal/product/handlers/rest/handler.go
@@ -4,26 +4,7 @@ import (
 	"net/http"
 
 	"github.com/JPauloMoura/controle-de-estoque/internal/product/services/product"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
-
-func Handler(svc product.ProductService) *chi.Mux {
-	h := newHandlerProduct(svc)
-	router := chi.NewRouter()
-
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.RequestID)
-
-	router.Get("/products", h.ListProducts)
-	router.Get("/products/{id}", h.GetProduct)
-	router.Post("/products", h.CreateProduct)
-	router.Put("/products/{id}", h.UpdateProduct)
-	router.Delete("/products/{id}", h.DeleteProduct)
-
-	return router
-}
 
 type HandlerProduct interface {
 	CreateProduct(w http.ResponseWriter, r *http.Request)
@@ -37,7 +18,7 @@ type handlerProduct struct {
 	svcProduct product.ProductService
 }
 
-func newHandlerProduct(svcProduct product.ProductService) HandlerProduct {
+func NewHandlerProduct(svcProduct product.ProductService) HandlerProduct {
 	return handlerProduct{
 		svcProduct: svcProduct,
 	}
